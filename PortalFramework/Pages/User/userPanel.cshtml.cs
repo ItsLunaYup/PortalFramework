f84937d877153e9bf1881fae376b830f0c94ebe1
Rick Downey <rick-downey@hotmail.com>
2022-12-09 22:34:33 +0100

    Added Base Files

---
+++ PortalFramework/Pages/User/userPanel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PortalFramework.Pages
+{
+    public class userPanelModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
